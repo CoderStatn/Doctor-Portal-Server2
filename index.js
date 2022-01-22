29ac3b54fa22909988d5087f07475420a979a8c6
nayeemtomal <80421010+nayeemtomal@users.noreply.github.com>
2022-01-22 21:55:04 +0600

    added doctor

--- index.js
+++ index.js
@@ -6,9 +6,11 @@ const { use } = require("express/lib/application");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 const stripe = require("stripe")(process.env.STRIPE_SECRET);
+const fileUpload = require("express-fileupload");
 
 app.use(cors());
 app.use(express.json());
+app.use(fileUpload());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xdqit.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -22,6 +24,7 @@ async function run() {
     const database = client.db("doctors_portal");
     const appointmentsCollection = database.collection("appointments");
     const usersCollection = database.collection("users");
+    const doctorsCollection = database.collection("doctors");
 
     app.get("/appointments", async (req, res) => {
       const email = req.query.email;
@@ -56,6 +59,28 @@ async function run() {
       };
       const result = await appointmentsCollection.updateOne(filter, updateDoc);
       res.json(result);
+    });
+
+    app.post("/doctors", async (req, res) => {
+      const name = req.body.name;
+      const email = req.body.email;
+      const pic = req.files.image;
+      const picData = pic.data;
+      const encodedPic = picData.toString("base64");
+      const imageBuffer = Buffer.from(encodedPic, "base64");
+      const doctor = {
+        name,
+        email,
+        image: imageBuffer,
+      };
+      const result = await doctorsCollection.insertOne(doctor);
+      res.json(result);
+    });
+
+    app.get("/doctors", async (req, res) => {
+      const cursor = doctorsCollection.find({});
+      const doctors = await cursor.toArray();
+      res.json(doctors);
     });
 
     app.get("/users/:email", async (req, res) => {
@@ -103,7 +128,7 @@ async function run() {
       } else {
         res
           .status(403)
-          .json({ message: "you don't have access to make admin" });
+          .json({ message: "You don't have access to make admin" });
       }
     });
     app.post("/create-payment-intent", async (req, res) => {
